b602690ddee8e15d66bff48ebaf2cdd05cba9644
Cristiano <jcristiano.c@gmail.com>
2023-09-20 15:27:06 -0300

    Removido componentes da homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,45 +91,7 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [              
-              {
-                label: 'GitHub',
-                href: 'https://github.com/jcristiano/fatec.pmi.sources',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        style: 'dark',                
       },
       prism: {
         theme: lightCodeTheme,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>        
       </div>
     </header>
   );
@@ -34,7 +27,7 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        
       </main>
     </Layout>
   );
